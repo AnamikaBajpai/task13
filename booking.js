fb5943502fd386fc34f2f537f0d2ebbd5b13f24b
AnamikaBajpai <125861298+AnamikaBajpai@users.noreply.github.com>
2023-03-01 20:13:09 +0530

    task13 done....

---
+++ booking.js
@@ -0,0 +1,104 @@
+
+//  const myForm = document.querySelector('#my-form');
+// // const nameInput =document.querySelector('#name');
+// // const emailInput = document.querySelector('#email');
+// // const phoneInput = document.querySelector('#phone');
+// // const msg =document.querySelector('.msg');
+// // const userList = document.querySelector('#users');
+
+ //myForm.addEventListener('submit' , onSubmit);
+
+function saveToLocalStorage(event) {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const phonenumber =event.target.phone.value;
+    
+    // localStorage.setItem('name' , name);
+    // localStorage.setItem('email' , email);
+    // localStorage.setItem('phonenumber' , phonenumber);
+
+    const obj = {
+        name,
+        email,
+       phonenumber
+    }
+   localStorage.setItem(obj.email , JSON.stringify(obj.name + ' - ' + obj.email + ' - ' + obj.phonenumber));
+   showUserOnScreen(obj);
+
+}
+
+function showUserOnScreen(obj) {
+    const parentElem = document.getElementById('users');
+    const childElem = document.createElement('li');
+    childElem.textContent = obj.name + ' - ' + obj.email + ' - ' + obj.phonenumber;
+
+     const deleteButton = document.createElement('input');
+     deleteButton.type = "button";
+     deleteButton.value ="Delete";
+
+     deleteButton.onclick =() => {
+        localStorage.removeItem(obj.email);
+        parentElem.removeChild(childElem);
+     }
+
+    childElem.appendChild(deleteButton);
+    parentElem.appendChild(childElem);
+ 
+    //parentElem.innerHTML = parentElem.innerHTML + '<li>$ {obj.name} - ${obj-email} - ${obj-phonenumber} </li>'
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function onSubmit(e) {
+//     e.preventDefault();
+
+//     if(nameInput.value ==='' || emailInput.value === '' || phoneInput.value ==='') {
+//         msg.classList.add('error');
+//         msg.innerHTML =' please enter all fields';
+
+//         setTimeout(() => msg.remove(),3000);
+      
+//     } else {
+        
+   
+
+//     // const li = document.createElement('li');
+//     // //li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value} : ${phoneInput.value}`));
+
+    
+//     // userList.appendChild(li);
+
+    // nameInput.value ='';
+    // emailInput.value ='';
+    // phoneInput.value ='';
+
+
+
+    
+// }
+
+
+// }
+// }
